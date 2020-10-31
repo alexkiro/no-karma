9c3f64d760d642183cf14811677da6e856ff6325
Alexandru Chirila <alex@alexkiro.me>
2020-10-31 15:39:09 +0200

    Add logging to gunicorn.

---
+++ docker/api/gunicorn.conf.py
@@ -0,0 +1,9 @@
+# https://docs.gunicorn.org/en/stable/settings.html#bind
+bind = "0.0.0.0:80"
+# https://docs.gunicorn.org/en/stable/settings.html#accesslog
+accesslog = "-"
+# https://docs.gunicorn.org/en/stable/settings.html#workers
+# Normally set in the .env.local file
+# workers = 1
+
+
