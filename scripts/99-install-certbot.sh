e50caf1674e3071f59aa7b84063e43e312991760
Alexandru Chirila <alex@alexkiro.me>
2020-11-03 08:53:54 +0200

    Enable http2

--- scripts/99-install-certbot.sh
+++ scripts/99-install-certbot.sh
@@ -7,3 +7,5 @@ certbot install --nginx --cert-name "$VUE_APP_HOST" -n || echo ""
 certbot install --nginx --cert-name "$VUE_APP_API_HOST" -n || echo ""
 # If successful the previous commands would will start nginx prematurely, stop it here
 nginx -s stop && sleep 1 || echo ""
+# Enable http2, see https://github.com/certbot/certbot/issues/3646
+sed -i "s/listen 443 ssl;/listen 443 ssl http2;/" /etc/nginx/conf.d/*.conf
--- scripts/run-certbot.sh
+++ scripts/run-certbot.sh
@@ -4,3 +4,5 @@ set -evx
 
 certbot --nginx -n --agree-tos --email "$CERTBOT_EMAIL" -d "$VUE_APP_HOST"
 certbot --nginx -n --agree-tos --email "$CERTBOT_EMAIL" -d "$VUE_APP_API_HOST"
+# Enable http2, see https://github.com/certbot/certbot/issues/3646
+sed -i "s/listen 443 ssl;/listen 443 ssl http2;/" /etc/nginx/conf.d/*.conf
