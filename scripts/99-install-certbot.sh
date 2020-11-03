e48b64b81011c51a1a65ed09a1735f9c684ff62a
Alexandru Chirila <alex@alexkiro.me>
2020-11-03 23:28:19 +0200

    Run certbot for all hosts.

--- scripts/99-install-certbot.sh
+++ scripts/99-install-certbot.sh
@@ -2,9 +2,11 @@
 
 set -evx
 
-# Check if any certificates are already available and install them into the nginx conf
-certbot install --nginx --cert-name "$VUE_APP_HOST" -n || echo ""
-certbot install --nginx --cert-name "$VUE_APP_API_HOST" -n || echo ""
+grep -rh "server_name" /etc/nginx/conf.d/ | tr ';' ' ' | awk '{ print $2 }' | sort | uniq | while read line; do
+  # Check if any certificates are already available and install them into the nginx conf
+  certbot install --nginx --cert-name "$line" -n || echo ""
+done
+
 # If successful the previous commands would will start nginx prematurely, stop it here
 nginx -s stop && sleep 1 || echo ""
 # Enable http2, see https://github.com/certbot/certbot/issues/3646
