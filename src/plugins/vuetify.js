722743be1a233de86109856f5fe3987dbbc2e747
Alexandru Chirila <alex@alexkiro.me>
2020-11-13 18:43:42 +0200

    Don't crate the emtpy style el.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,9 +3,12 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
+const plugin = new Vuetify({
   theme: { disable: true },
   icons: {
     iconfont: "md",
   },
 });
+// Monkey patch the function that generates the element.
+plugin.framework.theme.genStyleElement = () => {};
+export default plugin;
