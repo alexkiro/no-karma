da63b6117b196ef242580a64aa13ac4df066d854
Alexandru Chirila <alex@alexkiro.me>
2021-11-07 14:32:49 +0200

    Use dicebear avatars for subreddit placeholders

--- src/lib/mixins/utils.js
+++ src/lib/mixins/utils.js
@@ -3,7 +3,10 @@ const imageExtensions = new Set(["png", "jpg", "jpeg", "webp", "gif", "svg"]);
 export default {
   methods: {
     getSubRedditIcon(sub) {
-      return sub && (sub.community_icon || sub.icon_img);
+      return (
+        (sub && (sub.community_icon || sub.icon_img)) ||
+        `${this.$store.getters.avatarBase}/api/initials/${sub.display_name}.svg`
+      );
     },
     looksLikeImageUrl(url) {
       try {
